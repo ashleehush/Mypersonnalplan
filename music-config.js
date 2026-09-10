549526bbe525aff292c0d642adb16c7905d98b89
ashleehush <ashleymubama@outlook.fr>
2026-09-10 23:30:16 +0200

    Update music-config.js

--- music-config.js
+++ music-config.js
@@ -14,9 +14,9 @@
 
    Si les deux sont vides, la carte "Musique du jour" reste masquée.
    ========================================================================= */
-const spotifyPlaylistUrl = "https://youtube.com/watch?v=kNWiu6BNcEA&is=uummZSK6VEgj0WG-";
+const spotifyPlaylistUrl = "";
 // Exemple : "https://open.spotify.com/playlist/1h4ZXfRWnMjV7XLy7MH8yO?si=U17R__CBSq6S6wtY1E9dTw&utm_source=copy-link&pi=AhMuucJRTtWw3"
 
 const musicLinks = [
-  // "https://www.youtube.com/watch?v=XXXXXXXXXXX",
+  // "https://youtube.com/watch?v=kNWiu6BNcEA&is=uummZSK6VEgj0WG-",
 ];
